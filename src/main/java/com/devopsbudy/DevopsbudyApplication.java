37572ae82298c934267105651f694b1b27901659
Matis <mateusz.rostalski@gmail.com>
2018-05-29 15:35:46 +0200

    initial project commit

---
+++ src/main/java/com/devopsbudy/DevopsbudyApplication.java
@@ -0,0 +1,12 @@
+package com.devopsbudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsbudyApplication.class, args);
+    }
+}
