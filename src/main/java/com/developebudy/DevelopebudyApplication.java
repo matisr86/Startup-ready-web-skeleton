71e796cb3757ca3bbb3c625a5587e544efdb2dfb
Matis <mateusz.rostalski@gmail.com>
2018-05-30 12:52:25 +0200

    initialize project

---
+++ src/main/java/com/developebudy/DevelopebudyApplication.java
@@ -0,0 +1,12 @@
+package com.developebudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopebudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopebudyApplication.class, args);
+    }
+}
